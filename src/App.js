1f71db8ede3e593bae5cea918dac982db62a3030
Nick <enviouscodefellow@outlook.com>
2022-10-24 23:56:29 -0700

    not complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import Axios from 'axios';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: [],
+
+    }
+  }
+
+  handleGetCity = (event) => {
+    event.preventDefault();
+    let cityData = axios.get('https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json')
+  }
+
+  handleInput = (event) => {
+    event.preventDefault();
+    this.setState({
+      city: event.target.value
+    })
+  }
+
+  getCityData = (event) => {
+    event.preventDefault();
+  }
+
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>API Call</h1>
+
+    <form>
+      <button onClick={this.handleGetCity}>Click Me!</button>
+    </form>
+    </>
+  )
 }
 
 export default App;
